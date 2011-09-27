5a3359c0b5887f8ef68caa415aa51b7786a05f77
nofatclips <nofatclips@gmail.com>
2011-09-27 19:01:37 +0000

    Aggiunto supporto agli ImageButton. Il crawler ora è impostato su Quick Settings: http://code.google.com/p/quick-settings/

--- AndroidCrawler/src/com/nofatclips/crawler/ButtonFilter.java
+++ AndroidCrawler/src/com/nofatclips/crawler/ButtonFilter.java
@@ -3,12 +3,13 @@ package com.nofatclips.crawler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 
 public class ButtonFilter extends ListFilter {
 
 	@Override
 	public boolean isValidItem(View v) {
-		return ((v instanceof Button) && (!(v instanceof CompoundButton)));
+		return ( ((v instanceof Button) && (!(v instanceof CompoundButton))) || (v instanceof ImageButton) );
 	}
 
 }

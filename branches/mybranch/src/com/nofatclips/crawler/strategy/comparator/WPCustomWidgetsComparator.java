75ba6833ce6f5b088d6bcc15fd78fb8204dd1068
castigliafrancesco <castigliafrancesco@gmail.com>
2012-01-17 14:37:39 +0000

    Aggiunto WPCustomWidgetsDeepComparator che confronta le activity di nome "Posts" valutando la proprietà  nome' del widget textView di id "2131165236" che nelle due activity risulta rispettivamente pari a "Pages" e "Posts". Modificata lista di valori per campi di tipo NUMBER DECIMAL.

---
+++ branches/mybranch/src/com/nofatclips/crawler/strategy/comparator/WPCustomWidgetsComparator.java
@@ -0,0 +1,95 @@
+package com.nofatclips.crawler.strategy.comparator;
+
+import java.util.HashSet;
+
+import android.util.Log;
+
+import com.nofatclips.androidtesting.model.ActivityState;
+import com.nofatclips.androidtesting.model.WidgetState;
+
+// Accetta in input (nel costruttore) un numero arbitrario di tipi di widget e compara le activity
+// considerandone il nome ed i widget dei tipi selezionati. Una activity A sarà diversa da B se il
+// nome di A è diverso da quello di B, o se fra i widget dei tipi selezionati posseduti da A ce
+// n'è almeno uno che B non possiede.
+// Sono presi in considerazione solo i widget dotati di ID.
+
+// Es: Comparator COMPARATOR = new CustomWidgetsComparator("button", "editText");
+
+public class WPCustomWidgetsComparator extends NameComparator {
+	
+	public final static boolean IGNORE_ACTIVITY_NAME = true;
+	private boolean byName = true;
+	protected String[] widgetClasses;
+	
+	public WPCustomWidgetsComparator (String... widgets) {
+		super ();
+		this.widgetClasses = widgets; 
+	}
+	
+	public WPCustomWidgetsComparator (boolean ignore, String... widgets) {
+		this(widgets);
+		this.byName = !ignore;
+	}
+	
+	public boolean matchClass (String type) {
+		for (String storedType: this.widgetClasses) {
+			if (storedType.equals(type)) return true;
+		}
+		return false;
+	}
+	
+	protected boolean matchWidget (WidgetState campo, WidgetState altroCampo) {
+		return ( (altroCampo.getId().equals(campo.getId())) && (altroCampo.getSimpleType().equals(campo.getSimpleType())));
+	}
+
+	@Override
+	public boolean compare(ActivityState currentActivity, ActivityState storedActivity) {
+		HashSet<String> checkedAlready = new HashSet<String>();
+
+		if (this.byName && !super.compare (currentActivity, storedActivity)) return false; // Different name, different activity, early return
+			
+		// Check if current has at least one widget that stored ain't got
+		Log.d("nofatclips","Looking for additional widgets");
+		for (WidgetState campo: currentActivity) {
+			int id = Integer.valueOf(campo.getId());
+			String type = campo.getSimpleType();
+			Log.d("nofatclips","Comparator found widget " + id + " (type = " + type + ")");
+					
+			boolean particullary =((currentActivity.getName().equals("Posts"))
+								 &&(campo.getId().equals("2131165236"))
+								 &&(campo.getSimpleType().equals("text"))
+								 &&((campo.getName().equals("Posts")||campo.getName().equals("Pages")))
+								  );
+			if ((matchClass(campo.getSimpleType()) && (id>0) )||(particullary)){
+				Log.v("nofatclips","Comparing " + type + " #" + id);
+				if (!lookFor(campo, storedActivity)) return false;
+				checkedAlready.add(campo.getId()); // store widgets checked in this step to skip them in the next step
+			}			
+		}
+		
+		// Check if stored has at least one widget that current ain't got. Skip if already checked.
+		Log.d("nofatclips","Looking for missing widgets");
+		for (WidgetState campo: storedActivity) {
+			int id = Integer.valueOf(campo.getId());
+			String type = campo.getSimpleType();
+			Log.d("nofatclips","Comparator found widget " + id + " (type = " + type + ")");
+			
+			if ( matchClass(campo.getSimpleType()) && (id>0) && (!checkedAlready.contains(campo.getId())) ) {
+				Log.v("nofatclips","Comparing " + type + " #" + id);
+				if (!lookFor(campo, currentActivity)) return false;
+			}
+		}
+		
+		return true; // All tests failed, can't found a difference between current and stored!
+	}
+	
+	protected boolean lookFor (WidgetState campo, ActivityState activity) {
+		for (WidgetState altroCampo: activity) {
+			if (matchWidget (altroCampo, campo)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}

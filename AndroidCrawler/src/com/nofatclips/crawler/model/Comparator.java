ff960ad2e67ae3eb8830785317b3d24669849881
nofatclips@gmail.com <nofatclips@gmail.com@d4753398-7b8b-f9b4-ad78-7a7c91244809>
2012-03-20 15:28:22 +0000

    Support for comparation_widgets properties of the SESSION Updated Comparator with a method to describe the "comparation widgets" Updated androidtest.jar to rev. 217

--- AndroidCrawler/src/com/nofatclips/crawler/model/Comparator.java
+++ AndroidCrawler/src/com/nofatclips/crawler/model/Comparator.java
@@ -5,5 +5,6 @@ import com.nofatclips.androidtesting.model.ActivityState;
 public interface Comparator {
 
 	public boolean compare (ActivityState a, ActivityState b);
+	public String describe();
 	
 }

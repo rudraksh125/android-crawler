46de46b7f1bf3523ec60b004119d0dbd4c3e0567
nicola.amatucci <nicola.amatucci@gmail.com>
2012-06-19 10:14:32 +0000

    SENSORI: Integrata "soluzione 1) con instrumentazione", aggiunta cache per la reflection

---
+++ src/com/nofatclips/crawler/automation/sensors_utils/ActivityReflectorCacheElement.java
@@ -0,0 +1,6 @@
+package com.nofatclips.crawler.automation.sensors_utils;
+
+public class ActivityReflectorCacheElement {
+	public Boolean usesSensors = null;
+	public Boolean usesLocation = null;
+}

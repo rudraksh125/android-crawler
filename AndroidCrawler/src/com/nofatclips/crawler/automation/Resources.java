7ce0ba48507de54c109d1b07b4f0ee096f1e0b59
nicola.amatucci <nicola.amatucci@gmail.com>
2012-10-16 16:14:20 +0000

    Commit Resources.java default (by Gennaro)

--- AndroidCrawler/src/com/nofatclips/crawler/automation/Resources.java
+++ AndroidCrawler/src/com/nofatclips/crawler/automation/Resources.java
@@ -10,24 +10,19 @@ public class Resources implements ResourceFile {
 
 	// Precrawling sequence
 	public static int SLEEP_AFTER_EVENT = 1000;
-	public static int SLEEP_AFTER_RESTART = 500;
-	public static int SLEEP_ON_THROBBER = 30000; // How long to wait on spinning wheels (in ms -- 0 = don't wait)
-	public static int SLEEP_AFTER_TASK = 0;
+	public static int SLEEP_AFTER_RESTART = 1000;
+	public static int SLEEP_ON_THROBBER = 1000; // How long to wait on spinning wheels (in ms -- 0 = don't wait)
+	public static int SLEEP_AFTER_TASK = 1000;
 
-	public static boolean FORCE_RESTART = true;
+	public static boolean FORCE_RESTART = false;
 	public static boolean IN_AND_OUT_FOCUS = true;
 
-	public static String[] PRECRAWLING = new String[] {
-//		CLICK_ON_TEXT, "Posts", null,
-//		LIST_LONG_SELECT, "2", null,
-//		CLICK_ON_TEXT, "Edit Post", null,
-//		CLICK_ON_TEXT, "Select Categories", null
-	};
+	public static String[] PRECRAWLING = new String[] {};
 
 	// Screenshot Parameters
 	public static boolean SCREENSHOT_FOR_STATES = true; // Performs an image capture of the screen after processing a task
 	public static boolean SCREENSHOT_FOR_EVENTS = false; // Performs an image capture of the screen before firing an event
-	public static boolean SCREENSHOT_ONLY_NEW_STATES = false; // Capture only if comparation was negative - Only relevant if SCREENSHOT_FOR_STATES is true
+	public static boolean SCREENSHOT_ONLY_NEW_STATES = true; // Capture only if comparation was negative - Only relevant if SCREENSHOT_FOR_STATES is true
 
 	static {
 		Prefs.updateNode("automation", Resources.class);
